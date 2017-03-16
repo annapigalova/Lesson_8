896dfc15ff3ca34ebb690d00fc95c49c32557bde
Anna Pigalova <annpigalova@gmail.com>
2017-03-16 17:06:24 +0300

    Lesson_8

--- src/Main.java
+++ src/Main.java
@@ -12,26 +12,26 @@ public class Main {
 		set1.addElement("B");
 		set1.addElement("C");
 		set1.addElement("A");
-		
+
 		Set<Integer> set2 = new Set<>();
 		set2.addElement(1);
 		set2.addElement(1);
 		set2.addElement(3);
 		set2.addElement(5);
-		
-		Comparator cmp = Collections.reverseOrder();  
-		
+
+		Comparator cmp = Collections.reverseOrder();
+
 		set1.sortElement(cmp);
 		set2.sortElement(cmp);
-		
-		for(int i = 0; i < set1.getSet().size(); i++)
+
+		for (int i = 0; i < set1.getSet().size(); i++)
 			System.out.println(set1.getSet().get(i));
-		
+
 		System.out.println("");
-		
-		for(int i = 0; i < set2.getSet().size(); i++)
+
+		for (int i = 0; i < set2.getSet().size(); i++)
 			System.out.println(set2.getSet().get(i));
-	
+
 	}
 
 }
